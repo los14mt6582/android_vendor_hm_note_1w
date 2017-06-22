b7258e2005a5e748f209f2b1162013523449cc2c
Monty Kumar <aryanmk.mk5@gmail.com>
2017-06-22 04:51:42 -0700

    Update and rename hm_note_1w-vendor.mk to irisX8-vendor.mk

--- hm_note_1w-vendor.mk
+++
@@ -1,9 +0,0 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Xiaomi/hm_note_1w/proprietary/bin,system/bin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Xiaomi/hm_note_1w/proprietary/etc,system/etc)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Xiaomi/hm_note_1w/proprietary/lib,system/lib)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Xiaomi/hm_note_1w/proprietary/xbin,system/xbin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Xiaomi/hm_note_1w/proprietary/vendor,system/vendor)
---
+++ irisX8-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LAVA/irisX8/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LAVA/irisX8/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LAVA/irisX8/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LAVA/irisX8/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/LAVA/irisX8/proprietary/vendor,system/vendor)
